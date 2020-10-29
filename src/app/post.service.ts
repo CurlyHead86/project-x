51642c2a3894b08650d1cf651c9ed4e712be7e74
CurlyHead86 <michiel.grauwels@gmail.com>
2020-10-29 02:32:40 +0100

    1 file modified

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,16 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PostService {
-
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   getPosts() {
-    return this.http.get('http://localhost/wordpress/wp-json/wp/v2/posts');
+    return this.http.get("http://localhost/wordpress/wp-json/wp/v2/posts");
+  }
+
+  authorize() {
+    this.http
+      .post("http://localhost/wordpress/wp-json/jwt-auth/v1/token", {
+        username = "curlyhead86",
+        password = "mollie"
+      })
+      .subscribe(data => {
+        console.log(data);
+        //if (data['token']) { // if token is returned
+
+        //this.token = data['token'];
+        //this.tokenChange.emit(this.token);
+      });
   }
 }
